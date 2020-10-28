e6d828b1958d318abeae29b424912d0ad72ac8f7
Kenneth <38896903+KennethHF@users.noreply.github.com>
2020-10-28 15:03:57 +0300

    Update gamelib_v2.js

--- gamelib_v2.js
+++ gamelib_v2.js
@@ -85,12 +85,21 @@ File Summary:
     image(img, x, y)
     draw(optGraphic)
     isMouseOver()
+  Sprite(imgObj, tileW, tileH)
+    width()
+    height()
+    at()
+    rows()
+    columns()
+    size()
+    move(x, y)
+    resize(w, h)
+    draw(optDestObj, optDestW, optDestH)
 
 
 
 
   Circle
-  Sprite
   HexGrid
   Grid
   pathfinder?
@@ -641,10 +650,74 @@ class Surface extends Rectangle {
 }
 
 
+class Sprite {
+  constructor(imgObj, tWidth, tHeight) {
+    this._width = tWidth;
+    this._height = tHeight;
+    this.imageObject = imgObj;
+    this.x = 0;
+    this.y = 0;
+    this.index = 0;
+    this.destWidth = tWidth;
+    this.destHeight = tHeight;
+  }
+
+  width() { return this._width; }
+  height() { return this._height; }
+  rows() { return (this.imageObject.height / this.height()); }
+  columns() { return (this.imageObject.width / this.width()); }
+  size() { return (this.rows() * this.columns()); }
+
+  move(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  resize(w, h) {
+    this.destWidth = w;
+    this.destHeight = h;
+  }
+
+  draw(optDestObj, optDestWidth, optDestHeight) {
+    if (this.index < 0 || this.index >= this.size()) return;
+    var dWidth = (optDestWidth == null ? this.destWidth : optDestWidth);
+    var dHeight = (optDestHeight == null ? this.destHeight : optDestHeight);
+
+    var srcX = 0;
+    var srcY = 0;
+    if (this.index != 0) {
+      srcX = (this.index % this.columns()) * this.width();
+      srcY = Math.floor(this.index / this.columns()) * this.height();
+    }
+
+    var destX = this.x;
+    var destY = this.y;
+    if (optDestObj != null) {
+      optDestObj.image(this.imageObject, destX, destY, dWidth, dHeight,
+      srcX, srcY, this.width(), this.height());
+    } else {
+      image(this.imageObject, destX, destY, dWidth, dHeight,
+      srcX, srcY, this.width(), this.height());
+    }
+  }
+}
 
 
+//**********************************************************************
 
+//**********************************************************************
 
+//**********************************************************************
+
+//**********************************************************************
+
+// STILL TO BE UPDATED BELOW *******************************************
+
+//**********************************************************************
+
+//**********************************************************************
+
+//**********************************************************************
 let Direction = {
   Invalid: -1,
   North: 0,
@@ -658,3 +731,549 @@ let Direction = {
   Left : 8,
   Right : 9,
 };
+
+
+class HexGrid {
+ constructor(col, rw, radius) {
+   this._radius = radius;
+   this._columns = col;
+   this._rows = rw;
+   this.x = 0;
+   this.y = 0;
+ }
+
+ //GETTERS
+ at()      { return new Vector(this.x, this.y); }
+ columns() { return this._columns; }
+ rows()    { return this._rows; }
+ radius()  { return this._radius; }
+ size()    { return (this._columns * this._rows); }
+ cellWidth()   { return (this._radius * 2); }
+ cellHeight()  { return (sqrt(3) * this._radius); }
+
+ width() {
+   let evenColumnsWidth = floor((this.columns() / 2) + 0.5) * (this.cellWidth());
+   let oddColumnsWidth = this.radius() * floor(this.columns() / 2);
+   if (this.columns() % 2 == 0) oddColumnsWidth += (this.radius() / 2);
+   return (evenColumnsWidth + oddColumnsWidth);
+ }
+ height() {
+   let evenColumnsHeight = (this.cellHeight() * this.rows());
+   if (this.columns() > 1) evenColumnsHeight += (this.cellHeight() / 2);
+   return evenColumnsHeight;
+ }
+
+ getBorderPoints() {
+  let pts = [];
+   //Add TOP points
+   for (let i = 0; i < this.columns(); i++) {
+     pts.push(this.cell(i).points[4]);
+     pts.push(this.cell(i).points[5]);
+   }
+   //Add RIGHT SIDE points
+   for (let i = this.columns() - 1; i < this.size(); i+=this.columns()) {
+     pts.push(this.cell(i).points[0]);
+     pts.push(this.cell(i).points[1]);
+     if (i == (this.size() - 1)) pts.push(this.cell(i).points[2]);
+   }
+   //Add BOTTOM points
+   for (let i = this.size() - 2; i >= (this.size() - this.columns()); i--) {
+     pts.push(this.cell(i).points[1]);
+     pts.push(this.cell(i).points[2]);
+   }
+   //Add LEFT SIDE points
+   for (let i = (this.size() - this.columns()); i >= 0; i-=this.columns()) {
+     pts.push(this.cell(i).points[3]);
+     if (i != 0) pts.push(this.cell(i).points[4]);
+   }
+  return pts;
+ }
+ 
+
+ //RETURNS A HEXAGON OBJECT WITH PROPERTIES
+ hex(xOrIndex, optionalY) {
+   if (arguments.length < 1 || arguments.length > 2) return null;
+   if (arguments[0] == null) return null;
+
+   //THIS OBJECT IS RETURNED IF VALID COORDINATES/INDEX PARAMETER
+   let h = {
+     x: 0,
+     y: 0,
+     index: null,
+     column: null,
+     row: null,
+     cube: {x:0, y:0, z:0}, 
+     width: this.cellWidth(),
+     height: this.cellHeight(),
+     points: []
+   }
+   
+
+   //SET EITHER THE INDEX OR COORDINATES ON THE GRID
+   if (arguments.length == 1) {
+     if (arguments[0] < 0 || arguments[0] >= this.size()) return null;
+     h.index = arguments[0];
+   } else {
+     if (arguments[0] < this.x || arguments[0] >= this.x + this.width()) return null;
+     if (arguments[1] < this.y || arguments[1] >= this.y + this.height()) return null;
+     h.x = arguments[0];
+     h.y = arguments[1];
+   }
+
+   //CALCUALTE THE REMAINING HEXAGON OBJECT PROPERTY VALUES
+   if (h.index != null) {
+
+     //WE HAVE THE INDEX; CALCULATE REST
+     h.column = h.index % this.columns();
+     h.row = floor(h.index / this.columns());
+     h.x = this.x + ((h.width / 2) + (h.width * 0.75 * h.column));
+     h.y = this.y + ((h.height / 2) + (h.height * h.row));
+     if (h.column % 2 != 0) h.y += (h.height / 2);
+     h.cube.x = h.column;
+     h.cube.z = h.row - (h.column - (h.column % 2)) / 2;
+     h.cube.y = -h.cube.x-h.cube.z;
+
+   } else {
+
+     //WE HAVE AN X,Y COORDINATE; CALCULATE REST
+     let col = (2/3 * (h.x - this.radius() - this.x)) / this.radius();
+     let row = (-1/3 * (h.x - this.radius() - this.x) + sqrt(3)/3 * (h.y - (h.height/2) - this.y)) / this.radius();
+     let rx = round(col);
+     let ry = round(-col-row);
+     let rz = round(row);
+
+     let x_dff = abs(rx - col);
+     let y_dff = abs(ry - (-col-row));
+     let z_dff = abs(rz - row);
+
+     if (x_dff > y_dff && x_dff > z_dff) {
+       rx = -ry-rz;
+     } else if (y_dff > z_dff) {
+       ry = -rx-rz;
+     } else {
+       rz = -rx-ry;
+     }   
+
+     h.column = rx;
+     if (h.column < 0 || h.column >= this.columns()) return null;
+
+     h.row = rz + (rx - (rx % 2)) / 2;
+     if (h.row < 0 || h.row >= this.rows()) return null;
+
+     h.x = this.x + ((h.width / 2) + (h.width * 0.75 * h.column));
+     h.y = this.y + ((h.height / 2) + (h.height * h.row));
+     h.index = h.row * this.columns() + h.column;
+     if (h.column % 2 != 0) h.y += (h.height / 2);
+     h.cube.x = h.column;
+     h.cube.z = h.row - (h.column - (h.column % 2)) / 2;
+     h.cube.y = -h.cube.x-h.cube.z;
+   }
+
+   //CALCUATE THE 6 POINTS OF THE HEXAGON
+   for (let v = 0; v < 6; v++) {
+     let angleDegree = 60 * v;
+     let angleRad = PI / 180 * angleDegree;
+     h.points.push(new Vector(h.x + this.radius() * cos(angleRad),
+                              h.y + this.radius() * sin(angleRad)));
+   }
+
+
+   //if (h.index < 0 || h.index >= this.size()) return null;
+   return h;
+ }
+
+ cell(xOrIndex, optionalY) {
+  if (arguments.length == 1) {
+    return this.hex(xOrIndex); 
+  } else {
+    return this.hex(xOrIndex, optionalY);
+  }
+ }
+
+
+ //DRAW SINGLE HEX
+ drawHex(hexObject, optDest) {
+   if (optDest != null) {
+     optDest.beginShape();
+     for (let i = 0; i < 6; i++)
+       optDest.vertex(hexObject.points[i].x, hexObject.points[i].y);
+     optDest.endShape(CLOSE);
+   } else {
+     beginShape();
+     for (let i = 0; i < 6; i++)
+       vertex(hexObject.points[i].x, hexObject.points[i].y);
+     endShape(CLOSE);
+   }
+ }
+
+
+ //DRAW ENTIRE GRID
+ draw() {
+   for (let i = 0; i < this.size(); i++)
+     this.drawHex(this.hex(i));
+ }
+
+}
+
+
+
+/*************************************************
+EVERYTHING BELOW THIS LINE IS FOR THE GRID CLASS
+*************************************************/
+
+//Node is used for the pathfinding
+//algorithm within the Grid class
+var NodeStatus = { NOTEVALUATED: 0, CLOSED: 1, OPEN: 2 };
+class Node {
+  constructor() {
+    this.parent = -1;
+    this.cost = 1;
+    this.scoreG = 0;
+    this.scoreH = 0;
+    this.scoreF = 0;
+    this.status = NodeStatus.NOTEVALUATED;
+  }
+}
+
+class Grid {
+  constructor(col, rw, cW, cH) {
+    this._columns = col;
+    this._rows = rw;
+    this._cellWidth = cW;
+    this._cellHeight = cH;
+    this.x = 0;
+    this.y = 0;
+    this.allowHorizontalWrapping = false;
+    this.allowVerticalWrapping = false;
+  }
+
+  columns() { return this._columns; }
+  rows() { return this._rows; }
+  size() { return this._columns * this._rows; }
+  cellWidth() { return this._cellWidth; }
+  cellHeight() { return this._cellHeight; }
+  width() { return this._columns * this._cellWidth; }
+  height() { return this._rows * this._cellHeight; }
+
+  move(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  wrap(horiz, vert) {
+    if (arguments.length == 0) {
+      this.allowHorizontalWrapping = !this.allowHorizontalWrapping;
+      this.allowVerticalWrapping = !this.allowVerticalWrapping;
+    } else if (arguments.length == 1) {
+      this.allowHorizontalWrapping = horiz;
+      this.allowVerticalWrapping = horiz;
+    } else {
+      this.allowHorizontalWrapping = horiz;
+      this.allowVerticalWrapping = vert;
+    }
+  }
+
+  cell(xOrIndex, optionalY) {
+    if (arguments.length < 1 || arguments.length > 2) return null;
+    if (arguments[0] == null) return null;
+
+    //Cell object to be returned if passes error check
+    var c = {
+      x: null,
+      y: null,
+      index: null,
+      row: null,
+      column: null,
+      points: [],
+      width: this.cellWidth(),
+      height: this.cellHeight()
+    }
+
+    //Error check and set the arguments
+    if (arguments.length == 1) {
+      if (arguments[0] < 0 || arguments[0] >= this.size()) return null;
+      c.index = arguments[0];
+    } else {
+      if (arguments[0] < this.x || arguments[0] >= this.x + this.width()) return null;
+      if (arguments[1] < this.y || arguments[1] >= this.y + this.height()) return null;
+      c.x = arguments[0];
+      c.y = arguments[1];
+    }
+
+    if (c.index != null) {
+      //We have the index (i) value, calculate the row,col
+      c.column = c.index % this.columns();
+      c.row = Math.floor(c.index / this.columns());
+    } else {
+      //We have a coordinate (x,y), calculate row,col,i
+      c.column = Math.floor((c.x - this.x) / c.width);
+      c.row = Math.floor((c.y - this.y) / c.height);
+      c.index = c.row * this.columns() + c.column;
+    }
+    c.x = this.x + (c.column * c.width);
+    c.y = this.y + (c.row * c.height);
+    c.points.push(new Vector(c.x, c.y));
+    c.points.push(new Vector(c.x + c.width, c.y));
+    c.points.push(new Vector(c.x + c.width, c.y + c.height));
+    c.points.push(new Vector(c.x, c.y + c.height));
+    return c;
+  }
+
+  neighborNorth(cellIndex) {
+    if (this.cell(cellIndex) == null) return null;
+    var n = cellIndex - this.columns();
+    if (n < 0 && this.allowVerticalWrapping) n += this.size();
+    return n;
+  }
+
+  neighborEast(cellIndex) {
+    if (this.cell(cellIndex) == null) return null;
+    var n = cellIndex + 1;
+    if (n % this.columns() == 0) {
+      if (this.allowHorizontalWrapping) {
+        n -= this.columns();
+      } else {
+        n = null;
+      }
+    }
+    return n;
+  }
+
+  neighborSouth(cellIndex) {
+    if (this.cell(cellIndex) == null) return null;
+    var n = cellIndex + this.columns();
+    if (n >= this.size() && this.allowVerticalWrapping) n -= this.size();
+    return n;
+  }
+
+  neighborWest(cellIndex) {
+    if (this.cell(cellIndex) == null) return null;
+    var n = cellIndex - 1;
+    if (cellIndex % this.columns() == 0) {
+      if (this.allowHorizontalWrapping) {
+        n = cellIndex + (this.columns() - 1);
+      } else {
+        n = null;
+      }
+    }
+    return n;
+  }
+
+  neighbor(aCell, direction) {
+    if (aCell == null) return null;
+    var c = aCell;
+
+    var n = null;
+    switch (direction) {
+      case (Direction.North):
+        n = this.neighborNorth(c.index);
+        break;
+      case (Direction.NorthEast):
+        n = this.neighborNorth(c.index);
+        n = this.neighborEast(n);
+        break;
+      case (Direction.East):
+        n = this.neighborEast(c.index);
+        break;
+      case (Direction.SouthEast):
+        n = this.neighborSouth(c.index);
+        n = this.neighborEast(n);
+        break;
+      case (Direction.South):
+        n = this.neighborSouth(c.index);
+        break;
+      case (Direction.SouthWest):
+        n = this.neighborSouth(c.index);
+        n = this.neighborWest(n);
+        break;
+      case (Direction.West):
+        n = this.neighborWest(c.index);
+        break;
+      case (Direction.NorthWest):
+        n = this.neighborNorth(c.index);
+        n = this.neighborWest(n);
+        break;
+    }
+    return this.cell(n);
+  }
+
+  draw() {
+   for (let i = 0; i < this.size(); i++)
+     rect(this.cell(i).x, this.cell(i).y, this.cellWidth(), this.cellHeight());
+  }
+
+  //Lowest F score function for pathfinding algorithm
+  getLowestFNodeIndex(nodesArray) {
+    var lowIndex = null;
+    for (var i = 0; i < nodesArray.length; i++) {
+      if (nodesArray[i].status == NodeStatus.OPEN) {
+        if (lowIndex == null) {
+          lowIndex = i;
+        } else {
+          lowIndex = (nodesArray[i].scoreF <= nodesArray[lowIndex].scoreF ? i : lowIndex);
+        }
+      }
+    }
+    return lowIndex;
+  }
+
+
+  //Compute G, H, and F scores for pathfinding algorithm
+  computeScores(nodes, nIndex, endIndex, allowDiag) {
+    var pIndex = nodes[nIndex].parent;
+
+    //Calculate G Value
+    nodes[nIndex].scoreG = 0;
+    if (pIndex != -1) {
+      nodes[nIndex].scoreG = nodes[pIndex].scoreG;
+
+      //If on same row/column as parent, add 10, otherwise add 14 (if diagonal allowed)
+      if (this.cell(pIndex).row == this.cell(nIndex).row ||
+        this.cell(pIndex).column == this.cell(pIndex).column) {
+        nodes[nIndex].scoreG += 10 * nodes[nIndex].cost;
+      } else {
+        nodes[nIndex].scoreG += (allowDiag == true ? (14 * nodes[nIndex].cost) : (20 * nodes[nIndex].cost));
+      }
+    }
+
+    //Calcualte H Value
+    nodes[nIndex].scoreH = 0;
+    var rowDist = Math.abs(this.cell(nIndex).row - this.cell(endIndex).row);
+    var colDist = Math.abs(this.cell(nIndex).column - this.cell(endIndex).column);
+    nodes[nIndex].scoreH = (rowDist < colDist ?
+      ((4 * rowDist) + (10 * colDist)) :
+      ((10 * rowDist) + (4 * colDist)));
+
+    //Calculate F Value
+    nodes[nIndex].scoreF = nodes[nIndex].scoreG + nodes[nIndex].scoreH;
+  }
+
+
+  //Returns A* path array
+  findPath(startIndex, endIndex, costArray, allowDiag) {
+    var path = [];
+    if (allowDiag == null) allowDiag = false;
+
+    if (startIndex < 0 || startIndex >= this.size()) return null;
+    if (endIndex < 0 || endIndex >= this.size()) return null;
+
+    //Create nodes
+    var nodes = [];
+    for (var i = 0; i < this.size(); i++) {
+      nodes.push(new Node());
+      var c = (costArray == null ? 1 : costArray[i]);
+      nodes[i].cost = c;
+    }
+
+    //Add start node to open list
+    nodes[startIndex].status = NodeStatus.OPEN;
+    this.computeScores(nodes, startIndex, endIndex, allowDiag);
+
+    var neighborCount = (allowDiag == false) ? 4 : 8;
+    var thisNode;
+    var thisNeighbor;
+
+    while (true) {
+      thisNode = this.getLowestFNodeIndex(nodes);
+      if (thisNode == null) return null;
+
+      nodes[thisNode].status = NodeStatus.CLOSED;
+      if (thisNode == endIndex) break;
+
+      //Check neighbors
+      for (var n = 0; n < neighborCount; n++) {
+        thisNeighbor = this.neighbor(this.cell(thisNode), n);
+        if (thisNeighbor == null) continue;
+
+        thisNeighbor = thisNeighbor.index;
+        if (nodes[thisNeighbor].cost > 0 && nodes[thisNeighbor].status != NodeStatus.CLOSED) {
+          //Check to see if neighbor is already open
+          if (nodes[thisNeighbor].status == NodeStatus.OPEN) {
+            //If it's on the open list, calculate new score and switch to lowest
+            var tmpNode = new Node();
+            tmpNode.parent = nodes[thisNeighbor].parent;
+            tmpNode.cost = nodes[thisNeighbor].cost;
+            tmpNode.scoreG = nodes[thisNeighbor].scoreG;
+            tmpNode.scoreH = nodes[thisNeighbor].scoreH;
+            tmpNode.scoreF = nodes[thisNeighbor].scoreF;
+            tmpNode.status = nodes[thisNeighbor].status;
+
+            this.computeScores(nodes, thisNeighbor, endIndex, allowDiag);
+            if (nodes[thisNeighbor].scoreG >= tmpNode.scoreG) {
+              nodes[thisNeighbor].parent = tmpNode.parent;
+              nodes[thisNeighbor].cost = tmpNode.cost;
+              nodes[thisNeighbor].scoreG = tmpNode.scoreG;
+              nodes[thisNeighbor].scoreH = tmpNode.scoreH;
+              nodes[thisNeighbor].scoreF = tmpNode.scoreF;
+              nodes[thisNeighbor].status = tmpNode.status;
+            }
+          } else {
+            //If neighbor is not already open
+            nodes[thisNeighbor].status = NodeStatus.OPEN;
+            nodes[thisNeighbor].parent = thisNode;
+            this.computeScores(nodes, thisNeighbor, endIndex, allowDiag);
+          }
+        }
+      }//end of check neighbors
+    } //end of while loop
+
+    //Path has been found if we are here
+    //Work backwards to determine path
+    var revPath = [];
+    thisNode = endIndex;
+    while (true) {
+      revPath.push(thisNode);
+      if (thisNode == startIndex) break;
+      thisNode = nodes[thisNode].parent;
+    }
+    for (var i = 0; i < revPath.length; i++)
+      path.push(revPath[(revPath.length - 1) - i]);
+
+    return path;
+  }
+}
+
+
+class Circle {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.rad = r;
+  }
+
+  radius() { return this.rad; }
+  center() { return new Vector(this.x, this.y); }
+
+  //Returns a point on the circumference of the circle
+  //at a specified (parameter) degree between 0 - 360
+  point(degree) {
+    let x = this.x + (this.radius() * cos(radians(degree)));
+    let y = this.y + (this.radius() * sin(radians(degree)));
+    return new Vector(x, y);
+  }
+
+  //Calculates a line between the circle center and the
+  //parameter points and returns the radians it passes through
+  //along the circle's circumference
+  rads(x, y) {
+   return atan2(y - this.y, x - this.x);
+  }
+
+  contains(x, y) {
+    let centerDist = dist(this.x, this.y, x, y);
+    return (centerDist <= this.radius());
+  } 
+
+  collides(circle2) {
+    let centerDist = dist(this.x, this.y, circle2.center().x, circle2.center().y);
+    return (centerDist <= (this.radius() + circle2.radius()));
+  }
+  
+  draw(optGraphic) {
+    if (optGraphic == null) {
+      ellipse(this.x, this.y, this.radius()*2, this.radius()*2);
+    } else {
+      optGraphic.ellipse(this.x, this.y, this.radius()*2, this.radius()*2);
+    }
+  }
+}
